6b1e3182fa4789889b09364c0ef6c6b30cf66cb4
Steven Burns <steven@stevenburns.net>
2024-04-06 14:20:58 -0400

    Fixed init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from .burnsAI_chat_codegpt import CodeGPTNode
+from .chat_codegpt import CodeGPTNode
 
-NODE_CLASS_MAPPINGS = { "burnsAI": CodeGPTNode }
+NODE_CLASS_MAPPINGS = { "comfyUIAI": CodeGPTNode }
 
-NODE_DISPLAY_NAME_MAPPINGS = { "burnsAI": "CodeGPT Chat Node" }
+NODE_DISPLAY_NAME_MAPPINGS = { "comfyUIAI": "CodeGPT Chat Node" }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
